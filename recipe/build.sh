6e3bd7650d57430816f1f13fceb7799ca4d07b22
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2017-04-24 16:43:23 +0100

    Add gettext requirement for NLS support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ./configure --prefix="$PREFIX"
+if [ $(uname -s) == 'Darwin' ]; then
+    # Modify Makefiles for gettext
+    sed -i.bak -e "s|^LDFLAGS =.*|LDFLAGS = -Wl,-rpath $PREFIX/lib|" gnulib-tests/Makefile Makefile
+fi
 make
 make -j 1 check
 make install
