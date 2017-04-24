82ee0771b02976a6ad54d8a12a4e141a9249e842
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2017-04-24 19:09:28 +0100

    Skip broken test under Linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,9 @@
 if [ $(uname -s) == 'Darwin' ]; then
     # Modify Makefiles for gettext
     sed -i.bak -e "s|^LDFLAGS =.*|LDFLAGS = -Wl,-rpath $PREFIX/lib|" gnulib-tests/Makefile Makefile
+else
+    # Remove test broken in sed 4.4
+    sed -i.bak -e 's|testsuite/panic-tests.sh||' Makefile
 fi
 make
 make -j 1 check
