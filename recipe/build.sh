ead6d7b647358f8cd9cf3470c608d79dbff758d8
Paul Yim <paul.j.yim@gmail.com>
2021-02-15 03:49:39 +0000

    Add bootstraping template; move deps under bootstrapping template

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
+
+if [[ ! $BOOTSTRAPPING == yes ]]; then
+  # Get an updated config.sub and config.guess
+  cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
+fi
 
 set -ex
 
